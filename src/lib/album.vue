6e12799c2fb89977a86df77451807ff3afb4edc4
weigucheng <weigucheng@i-hc.cn>
2024-08-09 18:35:00 +0800

    修改相册

--- src/lib/album.vue
+++ src/lib/album.vue
@@ -1,30 +1,27 @@
 <template>
-	<div>
-		<div class="mdl-album-detail-big-box" :style="style">
-			<!--
-				循环的时候，把所有内容全部循环到
-				但只展示 [first, first + 3) 之间的内容
-			-->
-			<div class="mdl-album-detail-btn-box">
-				<div :class="'btn-box ' + (leftDisabled ? 'disaebld-btn-box' : 'active-btn-box')" @click="onPrev">《&nbsp;&nbsp;</div>
-			</div>
-			<div class="mdl-album-detail-data-box" v-loading="loading">
-				<div v-if="itemsCount > 0">
-					<div v-for="index in itemsCount" :key="index"	v-if="index >= first && index < first + 3">
-						<div :class="(now === index) ? 'is-active' : 'not-active'" @click="onChangeShow(index)">
-							<slot :index="index"></slot>
-						</div>
-					</div>
-				</div>
-				<div v-else>
-					<div class="empty-data-box">
-						
-					</div>
+	<div class="mdl-album-big-box" :style="style">
+		<!--
+		 * 循环的时候，把所有主控全部循环到
+		 * 但只展示 [first, first + 3) 之间的内容
+		-->
+		<div class="mdl-album-btn-box">
+			<div :class="'btn-box ' + (leftDisabled ? 'disaebld-btn-box' : 'active-btn-box')" @click="onPrev">《&nbsp;&nbsp;</div>
+		</div>
+		<div class="mdl-album-data-box">
+			<div v-if="total > 0">
+			<div v-for="index in total" :key="index"  v-if="index - 1 >= first && index - 1 < first + 3">
+				<div :class="(now === (index - 1)) ? 'is-active' : 'not-active'" @click="onChangeShow(index - 1)">
+					<slot :index="index - 1"></slot>
 				</div>
 			</div>
-			<div class="mdl-album-detail-btn-box">
-				<div :class="'btn-box ' + (rightDisabled ? 'disaebld-btn-box' : 'active-btn-box')" @click="onNext">&nbsp;&nbsp;》</div>
 			</div>
+			<div v-else>
+			<div class="empty-box">
+			</div>
+			</div>
+		</div>
+		<div class="mdl-album-btn-box right-btn">
+			<div :class="'btn-box ' + (rightDisabled ? 'disaebld-btn-box' : 'active-btn-box')" @click="onNext">&nbsp;&nbsp;》</div>
 		</div>
 	</div>
 </template>
@@ -35,10 +32,10 @@ export default {
 	name: "MdlAlbum",
 	mixins: [ StyleMixin ],
 	props: {
-		itemsCount: {
+		total: {
 			type: Number,
 			default: 0
-		},
+		}
 	},
 	data () {
 		return {
@@ -53,13 +50,13 @@ export default {
 	methods: {
 		/*
 		 * 按下一步的时候，向右前进一格，[ ○ □ □ ] □ □ □ => [ □ ○ □ ] □ □ □
-		 *如果已经到最右，则把看不见的向左推一格 [ □ □ ○ ] □ □ □	=> □ [ □ □ ○ ] □ □
+		 * 如果已经到最右，则把看不见的向左推一格 [ □ □ ○ ] □ □ □  => □ [ □ □ ○ ] □ □
 		 * 如果已经到最后，则什么都不做 □ □ □ [ □ □ ○ ]
-	
-		 * 按上一步的时候，向左前进一格，	□ □ □ [ □ ○ □ ] <= □ □ □ [ □ □ ○ ]
+		 *
+		 * 按上一步的时候，向左前进一格，  □ □ □ [ □ ○ □ ] <= □ □ □ [ □ □ ○ ]
 		 * 如果已经到最左，则把看不见的向右推一格 □ □ [ ○ □ □ ] □ <= □ □ □ [ ○ □ □ ] 
 		 * 如果已经到最前，则什么都不做 [ ○ □ □ ] □ □ □
-	
+		 *
 		 * 初始值（first）= 0
 		 * 显示范围：[first, first + 3)
 		 * 现在位置（ position ） = 0
@@ -67,94 +64,80 @@ export default {
 		onChangeShow (index) {
 			this.now = index;
 			this.position = index - this.first;
-			if (this.first >= 0) {
-				this.leftDisabled = false;
-			} else {
-				this.leftDisabled = true;
-			}
-			if (this.first < this.itemsCount - 1) {
-				this.rightDisabled = false;
-			} else {
-				this.rightDisabled = true;
-			}
 		},
+		/*
+		 * 点击下一步：
+		 * now++
+		 * position++
+		 * 如果 position > 2 （超出显示）
+		 * first ++
+		 * position = 2
+		 * 否则 first = 0
+		 */
 		onNext () {
-	
 			if (this.rightDisabled) return;
-		
-			/*
-			* 点击下一步：
-			* now++
-			* position++
-			* 如果 position > 2 （超出显示）
-			* first ++
-			* position = 2
-			* 否则 first = 0
-			*/
+
 			this.leftDisabled = false;
-			if (this.itemsCount === 0)	return;
+			if (this.total === 0)  return;
+
 			this.now++;
-			if (this.now === this.itemsCount - 1) {
-				this.rightDisabled = true;
-			}
-			if (this.now === this.itemsCount) {
-				this.now = this.itemsCount - 1;
-				this.rightDisabled = true;
+			if (this.now === this.total) {
+				this.now =  this.total - 1;
 				this.$Message({
 					message: "后面没有了",
-					type: "danger"
+					type: "info"
 				});
+				this.rightDisabled = true;
 				return;
 			}
+
 			this.position++;
 			if (this.position > 2) {
 				this.position = 2;
 				this.first++;
-				if (this.first > this.itemsCount - 3) {
-					this.first = this.itemsCount - 3;
+				if (this.first > this.total - 3) {
+					this.first = this.total - 3;
 					this.rightDisabled = true;
 					this.$Message({
 						message: "后面没有了",
-						type: "danger"
+						type: "info"
 					});
 				}
 			}
 		},
+		/*
+		 * 点击上一步
+		 * now--
+		 * poition--
+		 * 如果 position < 0 （超出显示）
+		 * position = 0
+		 * first --
+		 * 如果 fisrt < 0 => first - 0
+		 * 否则
+		 * first = 0
+		 */
 		onPrev () {
-	
 			if (this.leftDisabled) return;
-		
-			/*
-			* 点击上一步
-			* now--
-			* poition--
-			* 如果 position < 0 （超出显示）
-			* position = 0
-			* first --
-			* 如果 fisrt < 0 => first - 0
-			* 否则
-			* first = 0
-			*/
+
 			this.rightDisabled = false;
-			if (this.itemsCount === 0)	return;
+			if (this.total === 0)  return;
+
 			this.now--;
-			if (this.now === 0) {
-				this.leftDisabled = true;
-			}
 			if (this.now < 0) {
 				this.now = 0;
-				this.leftDisabled = true;
 				this.$Message({
 					message: "前面没有了",
-					type: "danger"
+					type: "info"
 				});
+				this.leftDisabled = true;
 				return;
 			}
+
 			this.position--;
 			if (this.position < 0) {
 				// □ □ □ [ ○ □ □ ]
 				this.position = 0;
-		
+
 				// 向左移动的时候，一般情况下只要 first-- 即可，直到 first 为 0
 				this.first--;
 				if (this.first < 0) {
@@ -162,36 +145,33 @@ export default {
 					this.leftDisabled = true;
 					this.$Message({
 						message: "前面没有了",
-						type: "danger"
+						type: "info"
 					});
 				}
 			}
 		}
 	}
-	}
+}
 </script>
 <style lang="scss" scoped>
-.mdl-album-detail-big-box {
+.mdl-album-big-box {
 	>div {
 		display: inline-block;
 		vertical-align: middle;
-		// height: 450px;
 	}
-	
-	.mdl-album-detail-btn-box {
+
+	.mdl-album-btn-box {
 		width: 40px;
 		text-align: center;
-	
+
 		.btn-box {
-			// margin-top: calc((78vh - 170px) / 2);
 			display: block;
 			height: 40px;
 			line-height: 40px;
-		
 			background: rgba($color: #CCCCCC, $alpha: 0.5);
 			border-radius: 50%;
 		}
-	
+
 		.active-btn-box {
 			cursor: pointer;
 		}
@@ -205,27 +185,28 @@ export default {
 			cursor: not-allowed;
 		}
 	}
-	
-	.mdl-album-detail-data-box {
+
+	.mdl-album-data-box {
 		width: calc(100% - 80px);
+		height: 100%;
 		>div {
 			height: 100%;
 			>div {
 				width: 33.33%;
 				height: 100%;
 				display: inline-block;
-				margin-top: 40px;
 				vertical-align: top;
 				>div {
 					margin: 0px 10px;
-					height: calc(100% - 50px);
+					height: 100%;
 					cursor: pointer;
 					>div {
-						height: 100%;
+					height: 100%;
 					}
 				}
 			}
-			.empty-data-box {
+
+			.empty-master-box {
 				>div {
 					cursor: default !important;
 				}
@@ -233,13 +214,9 @@ export default {
 			}
 		}
 	}
-	
+
 	.is-active {
 		background: rgba($color: #17579e, $alpha: 0.4);
-	}
-	
-	.hidden-master-box {
-		display: none;
 	}
 }
 </style>
