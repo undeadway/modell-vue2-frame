4a1d0b16d34f68058c7cf97b41423bd6161b800f
weigucheng <weigucheng@i-hc.cn>
2024-08-14 18:17:53 +0800

    修改树的结构

--- src/lib/tree/index.vue
+++ src/lib/tree/index.vue
@@ -1,11 +1,15 @@
 <template>
 	<div>
-		
+		<tree-node v-for="(item, index) in data" :value="item.value" :key="index" :index="index" :children="item.children" :depth="0" />
 	</div>
 </template>
 <script>
+import TreeNode from "./node";
 export default {
 	name: "Mv2Tree",
+	components: {
+		TreeNode
+	},
 	props: {
 		data: {
 			type: Array,
--- src/lib/tree/node.vue
+++ src/lib/tree/node.vue
@@ -1,12 +1,18 @@
 <template>
 	<div>
-		<div>{{ value }}</div>
-		<node-tree v-for="(item, index) in children" :value="item.value" :key="index" :index="index" :children="item.children" />
+		<div @click="onClick" class="mv2-tree-value-box" :style="`padding-left:${20 + 15 * depth}px;`">
+			<div><div :class="`mv2-tree-value-icon ${visible ? 'mv2-tree-value-is-open' : 'mv2-tree-value-is-closed'}`"></div></div>
+			<div>{{ value }}</div>
+		</div>
+		<div class="mv2-tree-children-box" v-show="visible">
+			<mv2-tree-node v-for="(item, index) in children" :value="item.value" :key="index" :index="index" :children="item.children" :depth="depth + 1" />
+		</div>
 	</div>
 </template>
 <script>
 export default {
 	name: "Mv2TreeNode",
+	componentName: "Mv2TreeNode",
 	props: {
 		value: {
 			type: String,
@@ -19,7 +25,54 @@ export default {
 		children: {
 			type: Array,
 			default: []
+		},
+		depth: {
+			type: Number,
+			default: 0
+		}
+	},
+	data () {
+		return {
+			visible: false
+		}
+	},
+	methods: {
+		onClick () {
+			if (this.children.length > 0) {
+				this.visible = !this.visible;
+			}
 		}
 	}
 }
 </script>
+<style lang="scss" scoped>
+.mv2-tree-value-box {
+	cursor: pointer;
+	padding: 4px;
+	&:hover {
+		background-color: #f0f0f0;
+	}
+	>div {
+		display: inline-block;
+		vertical-align: middle;
+		&:first-child {
+			font-size: 12px;
+
+			.mv2-tree-value-icon {
+				padding: 4px;
+				transition: transform .3s ease-in-out;
+				&::before {
+					content: "▶";
+				}
+			}
+		}
+	}
+}
+.mv2-tree-value-is-open {
+	transform: rotate(90deg);
+}
+
+.mv2-tree-value-is-closed {
+	transform: rotate(0deg);
+}
+</style>
