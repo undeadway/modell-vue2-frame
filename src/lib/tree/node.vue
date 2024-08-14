5d5e44fb3b872a03d9178bdadac8b901a982f815
weigucheng <weigucheng@i-hc.cn>
2024-08-14 18:24:04 +0800

    修改箭头

--- src/lib/tree/node.vue
+++ src/lib/tree/node.vue
@@ -1,7 +1,8 @@
 <template>
 	<div>
 		<div @click="onClick" class="mv2-tree-value-box" :style="`padding-left:${20 + 15 * depth}px;`">
-			<div><div :class="`mv2-tree-value-icon ${visible ? 'mv2-tree-value-is-open' : 'mv2-tree-value-is-closed'}`">▶</div></div>
+			<div v-if="children.length === 0"></div>
+			<div v-else><div :class="`mv2-tree-value-icon ${visible ? 'mv2-tree-value-is-open' : 'mv2-tree-value-is-closed'}`">▶</div></div>
 			<div>{{ value }}</div>
 		</div>
 		<div class="mv2-tree-children-box" v-show="visible">
