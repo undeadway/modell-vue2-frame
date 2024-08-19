4b03e4b6941e988ec51439e56d0537f416c7c1c6
weigucheng <weigucheng@i-hc.cn>
2024-08-19 14:08:02 +0800

    添加页面

---
+++ src/lib/date-picker/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<div v-if="type === 'time'"></div>
+	</div>
+</template>
+<script>
+export default {
+	props: {
+		type: String,
+		default: "date" // date、datetime、time、
+	},
+	data () {
+		return {
+			value: ""
+		}
+	}
+}
+</script>
---
+++ src/lib/date-picker/time.vue
@@ -0,0 +1,73 @@
+<template>
+	<div>
+		<div>
+			<div class="arrow-icon-box">
+				<div class="arrow-up-icon">&lt;</div>
+			</div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div class="arrow-icon-box">
+				<div class="arrow-down-icon">&lt;</div>
+			</div>
+		</div>
+		<div>
+			<div class="arrow-icon-box">
+				<div class="arrow-up-icon">&lt;</div>
+			</div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div class="arrow-icon-box">
+				<div class="arrow-down-icon">&lt;</div>
+			</div>
+		</div>
+		<div>
+			<div class="arrow-icon-box">
+				<div class="arrow-up-icon">&lt;</div>
+			</div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div class="arrow-icon-box">
+				<div class="arrow-down-icon">&lt;</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	props: {
+		start: {
+			type: String,
+			default: ""
+		},
+		end: {
+			type: String,
+			default: ""
+		}
+	},
+	data () {
+		return {
+			hour: 0,
+			minuter: 0,
+			second: 0
+		}
+	}
+}
+</script>
+<style lang="scss" scoped>
+.arrow-icon-box {
+	width: 20px;
+	height: 20px;
+	padding: 5px;
+	font-size: 10px;
+
+	.arrow-up-icon {
+		transform: rotate(270deg);
+	}
+	.arrow-down-icon {
+		transform: rotate(90deg);
+	}
+}
+</style>
