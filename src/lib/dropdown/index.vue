a255544c07a59f631f689ba10de6b732dd6b57fa
weigucheng <weigucheng@i-hc.cn>
2024-10-21 14:30:38 +0800

    拆分功能

---
+++ src/lib/dropdown/index.vue
@@ -0,0 +1,95 @@
+<template>
+	<div :style="style" class="mv2-dropdown-box" @click="delMenuList">
+		<div @click.stop="" ref="dropdown">
+			<div @click.stop="addMenuList" ref="slot">
+				<slot></slot>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+import Vue from "vue";
+import StyleMixin from './../../mixins/style-mixin';
+import utils from "./../../utils/utils";
+import Mv2DropdownMenu from "./menu";
+
+const MenuConstructor = Vue.extend(Mv2DropdownMenu);
+const DROPDOWN_LIST = [];
+
+export default {
+	name: "Mv2Dropdown",
+	mixins: [ StyleMixin ],
+	props: {
+		data: {
+			type: Array,
+			default: []
+		},
+		position: { // 位置，下拉列表的对齐方式
+			type: String,
+			default: "left"
+		},
+		align: { // 列表中文字的对齐方式
+			type: String,
+			default: "left"
+		}
+	},
+	data () {
+		return {
+			visible: false,
+			menuStyle: ""
+		}
+	},
+	created () {
+		this.delMenuList();
+	},
+	mounted() {
+		// 监听点击事件，用于关闭下拉框
+		window.addEventListener('click', this.delMenuList);
+	},
+	beforeUnmount() {
+		// 移除点击事件监听器
+		window.removeEventListener('click', this.delMenuList);
+	},
+	methods: {
+		onClickItem (command) {
+			this.delMenuList();
+			this.$emit("command", command);
+		},
+		delMenuList () {
+			let dropDown = null;
+			while (!!(dropDown = DROPDOWN_LIST.shift())) {
+				document.body.removeChild(dropDown.$el);
+				dropDown.$destroy();
+			}
+		},
+		addMenuList () {
+
+			this.delMenuList(); // 删除其他的下拉列表
+
+			const offsetPosition = this.getAbsolutePositon("dropdown");
+			const size = this.getElementtSize("slot");
+
+			const menuStyle = {
+				display: "block",
+				"text-align": this.align,
+				width: "xxx", // 这里是故意写错值，避免下面被 100% 覆盖
+				[this.position]: `${offsetPosition[this.position]}px`,
+				top: `${offsetPosition.top + size.height + 2}px`, // 2是用来和上面的按钮空开一小段距离
+			};
+
+			const mv2DropdownMenu = new MenuConstructor();
+			mv2DropdownMenu.styles = menuStyle;
+			mv2DropdownMenu.data = this.data;
+
+			mv2DropdownMenu.$mount();
+			document.body.appendChild(mv2DropdownMenu.$el);
+			DROPDOWN_LIST.push(mv2DropdownMenu);
+		}
+	}
+}
+</script>
+<style lang="scss" scoped>
+.mv2-dropdown-box {
+	display: inline-block;
+}
+</style>
---
+++ src/lib/dropdown/menu.vue
@@ -0,0 +1,53 @@
+<template>
+	<div :style="style" class="mv2-dropdown-menu">
+		<div v-for="d in data">
+			<div v-if="d.divider" class="mv2-dropdown-menu-divider"></div>
+			<div class="mv2-dropdown-item" @click.stop="onClickItem(d.command)">{{ d.label }}</div>
+		</div>
+	</div>
+</template>
+<script>
+import StyleMixin from './../../mixins/style-mixin';
+export default {
+	name: "Mv2DropdownMenu",
+	mixins: [ StyleMixin ],
+	props: {
+		data: {
+			type: Array,
+			default: []
+		},
+		position: { // 位置，下拉列表的对齐方式
+			type: String,
+			default: "left"
+		},
+		align: { // 列表中文字的对齐方式
+			type: String,
+			default: "left"
+		}
+	}
+}
+</script>
+<style lang="scss" scoped>
+.mv2-dropdown-menu {
+	position: absolute; // 绑定到 body
+	padding: 5px;
+	border: 1px solid #CCCCCC;
+	border-radius: 6px;
+	background: #FFFFFF;
+	>div {
+		.mv2-dropdown-item {
+			padding: 6px 8px;
+			border-radius: 6px;
+			cursor: pointer;
+			white-space:nowrap;
+			&:hover {
+				background: #DDDDDD;
+			}
+		}
+	}
+	.mv2-dropdown-menu-divider {
+		border-bottom: 1px solid #CCCCCC;
+		margin: 4px;
+	}
+}
+</style>
