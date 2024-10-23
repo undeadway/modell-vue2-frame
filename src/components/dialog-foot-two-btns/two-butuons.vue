9849912ac5e4e0ba9762a61b1f707a1b52b0f79c
weigucheng <weigucheng@i-hc.cn>
2024-10-23 15:25:51 +0800

    添加新组件

---
+++ src/components/dialog-foot-two-btns/two-butuons.vue
@@ -0,0 +1,45 @@
+<!-- 这里的两个按钮是平等关系，没有 确认、取消的区分 -->
+<template>
+    <div :style="style" class="mv2-dialog-foot-box">
+        <mv2-button ref="leftBtn" class="left-btn" @click="lefeEvent">{{ leftText }}</mv2-button>
+        <mv2-button ref="rightBtn" class="right-btn" @click="rightEvent">{{ rightText }}</mv2-button>
+    </div>
+</template>
+<script>
+import Mv2Button from "./../lib/button";
+
+export default {
+    components: {
+        Mv2Button
+    },
+    props: {
+        leftText: {
+            type: String,
+            default: "左按钮"
+        },
+        rightText: {
+            type: String,
+            default: "右按钮"
+        },
+        leftEvent: {
+            type: Function,
+            default: null
+        },
+        rightEvent: {
+            type: Function,
+            default: null
+        },
+        style: {
+            type: String,
+            default: ""
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.left-btn, .right-btn {
+    max-width: 30%;
+    min-width: 50px;
+    margin: auto 5px;
+}
+</style>
