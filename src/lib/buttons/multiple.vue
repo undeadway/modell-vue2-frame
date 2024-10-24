c9d5d59e1505bd9d95a4503280ce1ff9b8a6db0c
weigucheng <weigucheng@i-hc.cn>
2024-10-24 09:40:43 +0800

    修改引用层级

--- src/lib/buttons/multiple.vue
+++ src/lib/buttons/multiple.vue
@@ -3,7 +3,7 @@
 	<div class="mv2-buttons-box">
 		<div class="mv2-buttons">
 			<mv2-button v-for="(btn, index) in list" :style="style" class="multiple-btn" :ref="`multipleBtn${index}`" :key="index"
-				@click="btn.event()">{{ btn.text || `按钮${index}` }}</mv2-button>
+				@click="onClick(btn, index)">{{ btn.text || `按钮${index + 1}` }}</mv2-button>
 		</div>
 	</div>
 </template>
@@ -26,6 +26,12 @@ export default {
 	},
 	created () {
 		this.styles.width = `calc(100% / ${this.list.length} - 10px);`;
+	},
+	methods: {
+		onClick({event, text}, index) {
+			text = text || `按钮${index + 1}`;
+			event(this.$parent.$parent.$parent, text);
+		}
 	}
 }
 </script>
